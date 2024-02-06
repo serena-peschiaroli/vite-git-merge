4b064982ec1782c0a797433e06250249a59cd9c6
serena-peschiaroli <serena.peschiaroli@gmail.com>
2024-02-06 17:19:19 +0100

    Add: index rendering

---
+++ src/components/CocktailCard.vue
@@ -0,0 +1,43 @@
+<script>
+import { store } from '../store';
+
+export default {
+    props: {
+        cocktail: Object,
+        ingredients: {
+            type: Array,
+
+        }
+    },
+    data() {
+        return {
+            store,
+
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ cocktail.nome }}</h5>
+            <h4 class="card-subtitle mb-2 text-muted"> Alcolico: {{ cocktail.e_alcolico ? 'sì' : 'no' }} </h4>
+            <div class="card-body">
+                <p class="card-text"> <strong>Prezzo: </strong> {{ cocktail.prezzo }}</p>
+                <p class="card-text" style="background-color: {{ cocktail.colore }}"> <strong>Colore: </strong> {{ cocktail.colore }}</p>
+                
+            </div>
+            
+            
+            <!-- <router-link class="btn btn-success"
+                :to="{ name: 'project-page', params: { id: cocktail.id } }">Dettagli</router-link> -->
+
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss"></style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,28 @@
 <script>
 import axios from 'axios';
 import { store } from '../store.js';
+import CocktailCard from '../components/CocktailCard.vue'
 export default {
     data() {
         return {
             store,
             cocktails: [],
+            isLoading: true,
 
         }
+    },
+    created(){
+        axios.get(`${this.store.baseUrl}/api/cocktails`)
+        .then((resp)=>{
+            console.log(resp);
+            console.log(this.store.baseUrl);
+            this.cocktails = resp.data.results;
+            this.isLoading = false;
+
+        })
+    },
+    components: {
+        CocktailCard
     }
 }
 
@@ -16,7 +31,26 @@ export default {
 
 <template>
 
-    <h1> TEST</h1>
+    <div class="container">
+        <h2> La lista dei cocktail</h2>
+        <div v-if="isLoading" class="text-center mt-3">
+            <p>Caricamento in corso</p>
+            <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div v-else class="row row-cols-3 gy-5 mt-2">
+                <div class="col d-flex align-self-stretch" v-for="cocktail in cocktails" key="cocktail.id">
+                    <CocktailCard :cocktail="cocktail" />
+
+                </div>
+            
+            </div>
+        
+         
+    </div>
     
 
 </template>
