399dec2f58f3fd1fd0ad57d2e70712e360fba2c1
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2024-02-06 17:34:36 +0100

    add: show page

---
+++ src/pages/ShowCocktail.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from 'axios';
+import {store} from '../store';
+
+export default{
+    data(){
+        return{
+            currentCocktail: null,
+            store,
+            loading: false,
+        };
+    },
+    created(){
+        this.loading = true;
+
+        axios.get(`${this.store.baseUrl}/api/cocktails/${this.$route.params.id}`)
+        .then((resp) =>{
+            console.log(resp);
+
+            if(resp.data.success){
+                this.currentCocktail = resp.data.result;
+            }else{
+                console.log('---ERROR ---Cocktail non trovato');
+            }
+        }).finally(() =>{
+            this.loading = false;
+        })
+    }
+}
+
+</script>
+
+<template>
+    <h1>Show</h1>
+</template>
+
+<style lang="scss" scoped>
+</style>
